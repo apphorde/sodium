5ece96db89d34e4bbb09a988f021a746e62bbc69
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-05-05 15:45:39 +0200

    fix: split icon loader and component

---
+++ libraries/@lucide/icon-loader.mjs
@@ -0,0 +1,24 @@
+let ce;
+
+export async function createElement(name) {
+  if (!ce) {
+    const mod = await import(
+      "https://unpkg.com/lucide@latest/dist/esm/createElement.js"
+    );
+    ce = mod.default;
+  }
+
+  return ce(name);
+}
+
+export async function loadIcon(name) {
+  try {
+    const mod = await import(
+      `https://unpkg.com/lucide@latest/dist/esm/icons/${name}.js`
+    );
+    const icon = mod.default;
+    return createElement(icon);
+  } catch {
+    throw new Error("Icon not found: " + name);
+  }
+}
