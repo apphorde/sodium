695c1b1f8ae2f5b48548200373e613763fca0aed
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-05-29 13:43:56 +0530

    chore: add logs

--- publish.sh
+++ publish.sh
@@ -9,8 +9,9 @@ for scope in $(ls -1 $baseDir); do
   echo "Entering $scope"
   for component in $(ls -1 $baseDir/$scope); do
     name=$(echo $component | sed s/.html//)
-    echo -n "Publishing $scope/$name... "
+    echo -n "Publishing $scope/$name wrapper... "
     cat wrapper.mjs | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/component/$scope/$name@0.0.0 --data-binary @-
+    echo -n "Publishing $scope/$name template... "
     cat $baseDir/$scope/$component | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/library/$scope/$name.html@0.0.0 --data-binary @-
   done
 done
