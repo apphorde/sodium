7aeb640eca66072ba46284a5e17f78994bfca44f
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-05-29 14:14:51 +0530

    chore: remove extra logs

--- publish.sh
+++ publish.sh
@@ -10,9 +10,9 @@ for scope in $(ls -1 $baseDir); do
   for component in $(ls -1 $baseDir/$scope); do
     name=$(echo $component | sed s/.html//)
     echo -n "Publishing $scope/$name wrapper... "
-    cat wrapper.mjs | curl -v --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/component/$scope/$name@0.0.0 --data-binary @-
+    cat wrapper.mjs | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/component/$scope/$name@0.0.0 --data-binary @-
     echo -n "Publishing $scope/$name template... "
-    cat $baseDir/$scope/$component | curl -v --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/library/$scope/$name.html@0.0.0 --data-binary @-
+    cat $baseDir/$scope/$component | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/library/$scope/$name.html@0.0.0 --data-binary @-
   done
 done
 
