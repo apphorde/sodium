755a633082939de0823a32c64f9d961253d02c70
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-05-29 12:10:02 +0530

    chore: update publisher

--- publish.sh
+++ publish.sh
@@ -10,7 +10,8 @@ for scope in $(ls -1 $baseDir); do
   for component in $(ls -1 $baseDir/$scope); do
     name=$(echo $component | sed s/.html//)
     echo -n "Publishing $scope/$name... "
-    node wrapper.mjs $baseDir/$scope/$component | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/component/$scope/$name@0.0.0 --data-binary @-
+    cat wrapper.mjs | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/component/$scope/$name@0.0.0 --data-binary @-
+    cat $baseDir/$scope/$component | curl -sS --fail -H "Authorization: $CDN_API_KEY" $CDN_API_URL/library/$scope/$name.html@0.0.0 --data-binary @-
   done
 done
 
--- wrapper.mjs
+++ wrapper.mjs
@@ -1,14 +1,1 @@
-import { readFile } from "node:fs/promises";
-
-async function main() {
-  const [inputFile] = process.argv.slice(2);
-  const html = await readFile(inputFile, "utf-8");
-
-  console.log(`import { defineFromString } from "@li3/web";
-(async function(){const r = await (await fetch(import.meta.url)).text();defineFromString(r.slice(r.indexOf('/***'), -4));})();
-/***
-${html}
-***/`);
-}
-
-main();
+import {defineFromString} from "@li3/web";(async function(){const r = await (await fetch(import.meta.url+'.html')).text();defineFromString(r);})();
