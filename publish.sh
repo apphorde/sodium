44f76b0664f8933224e64f731aed53ed1ccfeb98
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-06-09 17:30:17 +0530

    fix: update components

--- publish.sh
+++ publish.sh
@@ -7,10 +7,11 @@ export PATH="./node_modules/.bin:$PATH"
 
 for scope in $(ls -1 $baseDir); do
   echo "Publishing components from $scope"
-  tar -cz -f - -C $baseDir/$scope --exclude node_modules/ --exclude .git/ --exclude tmp/ . | curl -sS --output - -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
+  tar -cz -f - -C $baseDir/$scope --exclude node_modules/ --exclude .git/ --exclude tmp/ . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
 done
 
 export baseDir=libraries
+echo "---"
 for scope in $(ls -1 $baseDir); do
   echo "Entering $scope"
   for library in $(ls -1 $baseDir/$scope); do
