559591ffc20670e9d0b25ef1498ed3630f3fe466
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-02 10:31:56 +0530

    fix: clean up code editor

--- publish.sh
+++ publish.sh
@@ -1,15 +1,15 @@
 set -e
 
 export PATH="./node_modules/.bin:$PATH"
-export componentsDir=components
 
 npm i @node-lambdas/cli
 mkdir tmpdir/
+cd components
 
-for library in $(ls -1 $componentsDir/*.mjs); do
-  cat $componentsDir/$library | fn es-minify --module true > tmpdir/$library
-  mv tmpdir/$library $componentsDir/$library
+for library in $(ls -1 *.mjs); do
+  cat $library | fn es-minify --module true > ../tmpdir/$library
+  mv ../tmpdir/$library ./$library
 done
 
-tar -cz -f - -C $componentsDir . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
+tar -cz -f - . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
 
