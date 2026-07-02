8c1174736eab889c42d249966ad8913b7d394704
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-02 08:19:28 +0530

    chore: simplify publishing

--- publish.sh
+++ publish.sh
@@ -1,23 +1,15 @@
 set -e
 
-npm i @node-lambdas/cli
 export PATH="./node_modules/.bin:$PATH"
 export componentsDir=components
-export libsDir=libraries
 
-echo "---"
-for scope in $(ls -1 $libsDir); do
-  echo "Entering $scope"
-  for library in $(ls -1 $libsDir/$scope); do
-    name=$(echo $library | sed s/.mjs//)
-    echo -n "Processing $scope/$name... "
-    cat $libsDir/$scope/$library | fn es-minify --module true > $componentsDir/$scope/$library
-  done
+npm i @node-lambdas/cli
+mkdir tmpdir/
+
+for library in $(ls -1 $componentsDir/*.mjs); do
+  cat $componentsDir/$library | fn es-minify --module true > tmpdir/$library
+  mv tmpdir/$library $componentsDir/$library
 done
 
-
-for scope in $(ls -1 $componentsDir); do
-  echo "Publishing components from $scope"
-  tar -cz -f - -C $componentsDir/$scope --exclude node_modules/ --exclude .git/ --exclude tmp/ . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
-done
+tar -cz -f - -C $componentsDir . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
 
