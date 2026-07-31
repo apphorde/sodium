4a2fa926c725fe20d8c83b261504dd116fd19eed
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2026-07-31 11:01:21 +0530

    chore: skip minification

--- publish.sh
+++ publish.sh
@@ -2,14 +2,14 @@ set -e
 
 export PATH="$PWD/node_modules/.bin:$PATH"
 
-npm i @node-lambdas/cli
-mkdir tmpdir/
+# npm i @node-lambdas/cli
+# mkdir tmpdir/
 cd components
 
-for library in $(ls -1 *.mjs); do
-  cat $library | fn es-minify --module true > ../tmpdir/$library
-  mv ../tmpdir/$library ./$library
-done
+# for library in $(ls -1 *.mjs); do
+  # cat $library | fn es-minify --module true > ../tmpdir/$library
+  # mv ../tmpdir/$library ./$library
+# done
 
 tar -cz -f - . | curl -sS -H 'Authorization: '$DEPLOY_API_KEY $DEPLOY_API_URL --data-binary @-
 
